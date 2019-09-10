e7b108ecd67c132e13d19dee0ccffc451aa98dcc
Varma Bhupatiraju <varmab@gmail.com>
2019-09-10 11:52:17 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
 
+import Counter from './Counter'
+
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <Welcome title="Ram"/>
+          <Welcome title="Ram" year={2020} isNew={true}/>
           <Welcome title="Varma"/>
         </header>
+        <Counter/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 
 class Welcome extends Component{
     constructor(){
@@ -17,9 +19,15 @@ class Welcome extends Component{
 
     render(){
         return(
-            <h1 onClick={this.updateClicks}>Welcome to { this.props.title } ({this.state.clicks})</h1>
+            <h1 onClick={this.updateClicks}>Welcome to { this.props.title } ({this.state.clicks}) - {this.props.year}</h1>
         )
     }
 }
+
+Welcome.propTypes={
+    title:PropTypes.string
+}
+
+
 
 export default Welcome;
